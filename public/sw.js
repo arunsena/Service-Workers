93acf7941c3d9deda0d33ffcbacbe20a54c08250
Arun Sena <arunsena123@gmail.com>
2018-09-25 00:01:48 -0400

    adding service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,39 @@
- 
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+   caches.open('service-worker-cache').then(function (cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/install.png',
+       '/register.png',
+       '/return.png',
+       '/update.png',
+       '/style.css'
+     ]);
+   })
+ );
+});
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+self.addEventListener('activate', function (event) {
+  console.log('Activating new service worker...');
+  const cacheWhitelist = ['service-worker-cache'];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+          return false;
+        })
+      );
+    })
+  );
+})
